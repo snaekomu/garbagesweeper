a781514deb66b2f32889181a04e25631adf74f1c
kam <ignasi.gv@outlook.es>
2019-11-03 23:21:19 +0200

    Initial Commit

---
+++ ma.sweeper/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ma.sweeper
+//
+//  Created by Ignasi Granell on 03/11/2019.
+//  Copyright © 2019 Cowb Oy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
